aba6c8d54495c85ad05fdb93f786d698a43776be
David Juarez <jdavidjuarez95@gmail.com>
2020-12-29 23:35:10 -0500

    STABLE / VERY MESSY - Added local storage and session retention NOT FINISHED
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -12,6 +12,7 @@ export class SessionService {
   private user: User = null;
   private tokenUpdated = new BehaviorSubject(null);
   private userUpdated = new BehaviorSubject(null);
+  private tokenTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {
   }
@@ -22,27 +23,6 @@ export class SessionService {
 
   getTokenUpdated() {
     return this.tokenUpdated.asObservable();
-  }
-
-  login(username: string, password: string) {
-    const credentials = {
-      username,
-      password
-    };
-    return this.http
-      .post<{ authUser: User, token: string }>('http://localhost:3000/api/users/login', credentials)
-      /*.pipe(
-      map(userData => {
-        return null
-      })x1
-    )*/
-      .subscribe((response) => {
-        console.log(response.authUser);
-        this.user = response.authUser;
-        this.tokenUpdated.next(response.token);
-        this.userUpdated.next(this.user);
-        this.router.navigate(['/home']);
-      });
   }
 
   registerUser(name: string, username: string, password: string, date: number) {
@@ -83,11 +63,85 @@ export class SessionService {
 
   }
 
+  login(username: string, password: string) {
+    const credentials = {
+      username,
+      password
+    };
+    return this.http
+      .post<{ authUser: User, token: string, expiresIn: number }>('http://localhost:3000/api/users/login', credentials)
+      /*.pipe(
+      map(userData => {
+        return null
+      })x1
+    )*/
+      .subscribe((response) => {
+        console.log(response.authUser);
+        const expiresInDuration = response.expiresIn;
+        this.setAuthTimer(expiresInDuration);
+        this.user = response.authUser;
+        this.tokenUpdated.next(response.token);
+        this.userUpdated.next(this.user);
+        const now = new Date();
+        const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+        console.log(expirationDate);
+        this.saveAuthData(response.authUser.id, response.token, expirationDate);
+        this.router.navigate(['/home']);
+      });
+  }
+
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    if (!authInformation) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.tokenUpdated.next(authInformation.token);
+      // this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      // this.authStatusListener.next(true);
+    }
+  }
+
   logout() {
     //  Set all authentication information to false when logging out
     this.tokenUpdated.next(null);
     this.userUpdated.next(null);
     this.user = null;
+    clearTimeout(this.tokenTimer);
+    this.clearAuthData();
+  }
+
+  private setAuthTimer(duration: number) {
+    console.log('Setting timer: ' + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
+  private saveAuthData(id: string, token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem('userID', id);
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token,
+      expirationDate: new Date(expirationDate)
+    };
   }
 
 }
