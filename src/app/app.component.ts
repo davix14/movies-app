09a8695de3bfab2f3a64fbfa8f3904081e3eb578
David Juarez <jdavidjuarez95@gmail.com>
2020-12-30 10:29:34 -0500

    STABLE / MESSY - Added autoAuthUser to app.component.ts to automatically use auth information in localstorage as soon as the application is reloaded
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SessionService} from './session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'movies-app';
+
+  constructor(private sessionService: SessionService) {
+  }
+
+  ngOnInit(): void {
+    this.sessionService.autoAuthUser();
+  }
 }
