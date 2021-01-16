4782685e301f99887f621b9e8c51a48816a18b27
David Juarez <jdavidjuarez95@gmail.com>
2021-01-15 22:10:28 -0500

    STABLE / CLEAN - Cleaned up the styling for the search results and added media queries for responsiveness
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SearchResult} from './searchResult.model';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-search',
@@ -8,12 +9,31 @@ import {SearchResult} from './searchResult.model';
 })
 export class SearchComponent implements OnInit {
   results: SearchResult[];
+  gridRowHeight = '50vh';
+  gridColNums = 1;
+
+  constructor(public breakPointObserver: BreakpointObserver) {
+  }
 
   ngOnInit() {
+    this.breakPointObserver.observe([
+      Breakpoints.Tablet,
+    ])
+      .subscribe(result => {
+        if (result.matches) {
+          // console.log('TABLET SCREEN DETECTED!');
+          this.gridRowHeight = '40vh';
+          this.gridColNums = 2;
+        } else {
+          this.gridRowHeight = '50vh';
+          this.gridColNums = 1;
+        }
+      });
   }
 
   getResults(results: SearchResult[]) {
     this.results = results;
     console.log(this.results);
   }
+
 }
