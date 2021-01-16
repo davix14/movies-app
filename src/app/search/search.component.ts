72327e9106393c8f18350b0a51c21cd80fd4645b
David Juarez <jdavidjuarez95@gmail.com>
2021-01-15 23:04:51 -0500

    STABLE / CLEAN - Added comments to the previous commit
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,24 +8,24 @@ import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  results: SearchResult[];
-  gridRowHeight = '50vh';
-  gridColNums = 1;
+  results: SearchResult[]; //  holds array of search results
+  gridRowHeight = '50vh'; //  Used to control the row height
+  gridColNums = 1; //  Used to control the column #s in the grid
 
-  constructor(public breakPointObserver: BreakpointObserver) {
+  constructor(public breakPointObserver: BreakpointObserver) { //  Injecting the breakpoint observer
   }
 
   ngOnInit() {
-    this.breakPointObserver.observe([
-      Breakpoints.Tablet,
+    this.breakPointObserver.observe([ //  Starting the breakPoint observer and breakpoints to check for below
+      Breakpoints.Tablet, //  Breakpoint for Tablet size
     ])
-      .subscribe(result => {
-        if (result.matches) {
-          // console.log('TABLET SCREEN DETECTED!');
-          this.gridRowHeight = '40vh';
-          this.gridColNums = 2;
-        } else {
-          this.gridRowHeight = '50vh';
+      .subscribe(result => { //  Subscribing to breakpoint activity - result provides breakpoints triggered (?)
+        if (result.matches) { //  Checkes if breakpoint sized received (result) matches current viewport size
+          // console.log(); //  For debugging
+          this.gridRowHeight = '40vh'; //  IF tablet : - Shrink rowHeight
+          this.gridColNums = 2; //  - Increase Col #
+        } else { // -IF not tablet:
+          this.gridRowHeight = '50vh'; //  Set rowHeight and Col # back to defaults
           this.gridColNums = 1;
         }
       });
@@ -33,7 +33,7 @@ export class SearchComponent implements OnInit {
 
   getResults(results: SearchResult[]) {
     this.results = results;
-    console.log(this.results);
+    // console.log(this.results);  For Debugging
   }
 
 }
