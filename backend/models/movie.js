cab988ed09e776c0d89daa1c504b507e1c6bc19a
David Juarez <jdavidjuarez95@gmail.com>
2021-01-19 23:37:48 -0500

    STABLE / MESSY - made the rating and the description optional
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
   title: { type: String, required: true },
-  rating: { type: Number, required: true },
-  description: { type: String, required: true },
+  rating: { type: Number, required: false },
+  description: { type: String, required: false },
   dateEntered: { type: Number, required: true },
   dateChanged: { type: Number, required: true },
   creator: { type: mongoose.Schema.Types.ObjectID, ref: "User", required: true }
