bddb7e95efecc8bfbbe3fdddbe251bd8a8a03c40
David Juarez <jdavidjuarez95@gmail.com>
2021-09-26 00:17:02 -0600

    STABLE / MESSY - separated the angular material imports into their own module
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,42 @@
+import {NgModule} from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatStepperModule
+  ]
+})
+export class AngularMaterialModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppHeaderComponent} from './app-header/app-header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {CreateMovieComponent} from './create-movie/create-movie.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 import {ListMoviesComponent} from './list-movies/list-movies.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatExpansionModule} from '@angular/material/expansion';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppHomeComponent} from './app-home/app-home.component';
 import {SearchComponent} from './search/search.component';
 import {SearchResultsComponent} from './search/search-results/search-results.component';
 import {SearchEntryComponent} from './search/search-entry/search-entry.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {LoginComponent} from './login/login.component';
 import {LoginFormComponent} from './login/login-form/login-form.component';
 import {NewUserComponent} from './new-user/new-user.component';
 import {RegisterFormComponent} from './new-user/register-form/register-form.component';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {AuthInterceptor} from './auth-interceptor';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatStepperModule} from '@angular/material/stepper';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {UserSettingsComponent} from './user-settings/user-settings/user-settings.component';
+import {AngularMaterialModule} from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -52,33 +35,18 @@ import {UserSettingsComponent} from './user-settings/user-settings/user-settings
     LoginFormComponent,
     NewUserComponent,
     RegisterFormComponent,
-    UserSettingsComponent
+    UserSettingsComponent,
+
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MatFormFieldModule,
-        ReactiveFormsModule,
-        MatInputModule,
-        MatSliderModule,
-        MatCardModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        MatExpansionModule,
-        HttpClientModule,
-        MatGridListModule,
-        MatDialogModule,
-        MatMenuModule,
-        MatProgressSpinnerModule,
-        MatSnackBarModule,
-        MatChipsModule,
-        MatStepperModule,
-        FlexLayoutModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    AngularMaterialModule
+  ],
   entryComponents: [
     CreateMovieComponent
   ],
